4d3d01b2100a2aac63dffbec6008f23cae4304f7
Tom Blackmore <tablackmore@gmail.com>
2018-10-16 21:35:33 +0200

    Adds test coverage report

---
+++ src/routes/NotFound/NotFound.test.js
@@ -0,0 +1,10 @@
+/* eslint-env jest */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import NotFound from './NotFound'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<NotFound />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
